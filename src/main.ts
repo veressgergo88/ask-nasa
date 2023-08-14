ec4dddb55a10d525d5992dbd5ebf3137e416c3bd
veressgergo88 <veress.gergo@gmail.com>
2023-08-14 18:36:45 +0200

    fixed code on the course

--- src/main.ts
+++ src/main.ts
@@ -10,68 +10,62 @@ const ApodResponseSchema = z.object({
   service_version: z.string(),
   title: z.string(),
   url: z.string().url(),
-  copyright: z.string().optional()
-})
+  copyright: z.string().optional(),
+});
 
-type ApodResponseSchema = z.infer<typeof ApodResponseSchema>
+type ApodResponse = z.infer<typeof ApodResponseSchema>;
 
-const apiDefaultLoad = async (): Promise<ApodResponseSchema | null> => {
-  const response = await http.get("https://api.nasa.gov/planetary/apod?api_key=WF3e0bbEjS5OAec5vUMc6carOEPjYpuQB9lIsZNf");
+const apiDefaultLoad = async (date: string): Promise<ApodResponse | null> => {
+  let response;
+  if (date) {
+    response = await http.get(
+      `https://api.nasa.gov/planetary/apod?date=${date}&api_key=WF3e0bbEjS5OAec5vUMc6carOEPjYpuQB9lIsZNf`
+    );
+  } else {
+    response = await http.get(
+      "https://api.nasa.gov/planetary/apod?api_key=WF3e0bbEjS5OAec5vUMc6carOEPjYpuQB9lIsZNf"
+    );
+  }
   const data = response.data;
-  console.log(data)
   const result = ApodResponseSchema.safeParse(data);
-  console.log(result)
-  
+
   if (!result.success) {
     console.error("Invalid API response:", result.error);
     return null;
-  }
-  else {
-    return result.data
+  } else {
+    return result.data;
   }
 };
 
+function renderData(apiData: ApodResponse) {
+  const datePicker = document.getElementById(
+    "dateSelector"
+  )! as HTMLInputElement;
+  datePicker.value = apiData.date;
+  document.getElementById("dayOfThePictureTitle")!.innerHTML = apiData.title;
+  const img = document.querySelector("img");
+  img!.src = apiData.url;
+  document.getElementById("dayOfThePictureExplanation")!.innerHTML =
+    apiData.explanation;
+  apiData.copyright !== undefined
+    ? (document.getElementById("dayOfThePictureAuthor")!.innerHTML =
+        apiData.copyright)
+    : (document.getElementById("dayOfThePictureAuthor")!.innerHTML = "");
+}
+
 const loadData = async () => {
-  const apiData = await apiDefaultLoad()
-  if(apiData){
-    const datePicker = document.getElementById("dateSelector")! as HTMLInputElement
-    datePicker.value = apiData.date
-    document.getElementById("dayOfThePictureTitle")!.innerHTML = apiData.title
-    const img = document.querySelector("img")
-    img!.src = apiData.url
-    document.getElementById("dayOfThePictureExplanation")!.innerHTML = apiData.explanation
-    apiData.copyright !== undefined ? document.getElementById("dayOfThePictureAuthor")!.innerHTML = apiData.copyright : document.getElementById("dayOfThePictureAuthor")!.innerHTML = ""
-  } 
-}
+  const apiData = await apiDefaultLoad("");
+  if (apiData) renderData(apiData);
+};
 
-loadData()
+loadData();
 
-const apiDateLoad = async (): Promise<ApodResponseSchema | null> => {
-  const dateValue = (document.getElementById("dateSelector")! as HTMLInputElement).value
-  const response = await http.get(`https://api.nasa.gov/planetary/apod?date=${dateValue}&api_key=WF3e0bbEjS5OAec5vUMc6carOEPjYpuQB9lIsZNf`);
-  const data = response.data;
-  console.log(data)
-  const result = ApodResponseSchema.safeParse(data);
-  console.log(result)
-  
-  if (!result.success) {
-    console.error("Invalid API response:", result.error);
-    return null;
-  }
-  else {
-    return result.data
-  }
-}
-
-document.getElementById("dateSelector")!.addEventListener("change", async () => {
-  const apiData = await apiDateLoad()
-  if(apiData){
-    const datePicker = document.getElementById("dateSelector")! as HTMLInputElement
-    datePicker.value = apiData.date
-    document.getElementById("dayOfThePictureTitle")!.innerHTML = apiData.title
-    const img = document.querySelector("img")
-    img!.src = apiData.url
-    document.getElementById("dayOfThePictureExplanation")!.innerHTML = apiData.explanation
-    apiData.copyright !== undefined ? document.getElementById("dayOfThePictureAuthor")!.innerHTML = apiData.copyright : document.getElementById("dayOfThePictureAuthor")!.innerHTML = ""
-  } 
-})
+document
+  .getElementById("dateSelector")!
+  .addEventListener("change", async () => {
+    const dateValue = (
+      document.getElementById("dateSelector")! as HTMLInputElement
+    ).value;
+    const apiData = await apiDefaultLoad(dateValue);
+    if (apiData) renderData(apiData);
+  });
